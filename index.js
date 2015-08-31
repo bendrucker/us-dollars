5b5219dd908ca5bdc6077fbcfb8b9c68a4bd782d
Ben Drucker <bvdrucker@gmail.com>
2015-08-31 13:38:12 -0700

    Delete console statement

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ var precision = require('precision')
 
 module.exports = function dollars (value, decimals) {
   decimals = decimals != null ? decimals : greatest(precision(value), 2)
-  console.log(greatest(4, 2))
   return '$' + value.toFixed(decimals)
 }
